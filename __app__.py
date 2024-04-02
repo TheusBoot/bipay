fa787800ba8271f2edf71098d1f329dfb605d8f3
Matheus <Emailmartingcf1@gmail.com>
2024-04-02 02:46:46 -0300

    ideia de negócio

---
+++ __app__.py
@@ -0,0 +1,8 @@
+
+#Necessário criar class de Pagamento (main)
+#Criação de sistema de carteira, possível que seja criado diversas carteiras no próprio código
+#transferencia para outra carteira de bitcoin, com os tipos de taxas.
+#consultar saldo antes da transferência e consultar saldo de carteiras 'aleatórias'.
+
+#Receber pagamentos, ou seja, emitir cobraças e conseguir verificar se foi pago ou não.
+
